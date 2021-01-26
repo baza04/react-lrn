1bc9aea430eba852e7a56607a1c59c26097467e2
baza04 <bazarbay.amir@gmail.com>
2021-01-26 22:15:32 +0600

    feat: start learn formik lyb

---
+++ Formik/formik-edu-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import SignupForm from "./components/form";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Form</h1>
+      <SignupForm />
+    </div>
+  );
+}
+
+export default App;
