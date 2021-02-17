b7d7069a5bf4b30ce51a3d0076b530dae19a06fb
baza04 <bazarbay.amir@gmail.com>
2021-02-17 19:35:35 +0600

    feat: start pdf saving

---
+++ pdf/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import PDF from "./component/PDF";
+import Post from "./component/Post";
+
+function App() {
+  return (
+    <div className="App" style={{ textAlign: "center" }}>
+      PDF
+      <Post />
+    </div>
+  );
+}
+
+export default App;
